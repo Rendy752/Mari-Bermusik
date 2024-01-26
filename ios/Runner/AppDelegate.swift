d3a77f1d1c2798b4f030f0bca2a3bdace58b13ce
Rendy Pratama <rendypratama752@gmail.com>
2024-01-27 02:06:45 +0700

    FIX: protect api key

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,25 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    // Load .env file
+    if let path = Bundle.main.path(forResource: ".env", ofType: nil) {
+        do {
+            let data = try String(contentsOfFile: path, encoding: .utf8)
+            let myStrings = data.components(separatedBy: .newlines)
+            let apiKey = myStrings[0].components(separatedBy: "=")[1]
+            
+            // Set API key in Info.plist
+            if let path = Bundle.main.path(forResource: "Info", ofType: "plist"), 
+               var dict = NSDictionary(contentsOfFile: path) as? [String: AnyObject] {
+                dict["API_KEY"] = apiKey as AnyObject
+                let newDict = NSDictionary(dictionary: dict)
+                newDict.write(toFile: path, atomically: true)
+            }
+        } catch {
+            print(error)
+        }
+    }
+    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -4,6 +4,23 @@ import FlutterMacOS
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+    if let path = Bundle.main.path(forResource: ".env", ofType: nil) {
+        do {
+            let data = try String(contentsOfFile: path, encoding: .utf8)
+            let myStrings = data.components(separatedBy: .newlines)
+            let apiKey = myStrings[0].components(separatedBy: "=")[1]
+            
+            // Set API key in Info.plist
+            if let path = Bundle.main.path(forResource: "Info", ofType: "plist"), 
+               var dict = NSDictionary(contentsOfFile: path) as? [String: AnyObject] {
+                dict["API_KEY"] = apiKey as AnyObject
+                let newDict = NSDictionary(dictionary: dict)
+                newDict.write(toFile: path, atomically: true)
+            }
+        } catch {
+            print(error)
+        }
+    }
     return true
   }
 }
